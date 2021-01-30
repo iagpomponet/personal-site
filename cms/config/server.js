002ff37f359fd4edcd05ffa06b769082ae090d1e
iagpomponet <iagosilbernagel@hotmail.com>
2021-01-29 22:50:58 -0300

    fix: fixed repository

--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e05b9b23105f2237888e56de0992d7bc'),
-    },
-  },
-});
--- cms/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '28cfa853-8372-468f-8085-23a2fea27cb5'
-};
